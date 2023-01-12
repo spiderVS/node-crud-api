0806c76b48c38e01fbe37f6b13820c6acdb0bb01
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2023-01-12 13:46:15 +0300

    feat: add new UsersDB class instead USERS constant

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,1 @@
-import { UserRecord } from "../modules/models/user.model";
-
 export const BASE_URL = '/api/users';
-export const USERS: UserRecord[] = [];
---
+++ src/database/users-db.ts
@@ -0,0 +1,34 @@
+import { UserRecord } from "../modules/models/user-record.model";
+
+export class UsersDB {
+  private _users: UserRecord[];
+  constructor() {
+    this._users = [];
+  }
+
+  get users(): UserRecord[] {
+    return this._users;
+  }
+
+  add(user: UserRecord): void {
+    this._users.push(user);
+  }
+
+  update(updatedUser: UserRecord): void {
+    const index = this.getIndexById(updatedUser.id);
+    this._users.splice(index, 1, updatedUser);
+  }
+
+  remove(user: UserRecord) {
+    const index = this.getIndexById(user.id);
+    this._users.splice(index, 1);
+  }
+
+  findUserById(id: string): UserRecord | null {
+    return this._users.find((user: UserRecord) => user.id === id) ?? null;
+  }
+
+  private getIndexById(id: string): number {
+    return this._users.findIndex((user: UserRecord) => user.id === id);
+  }
+}
