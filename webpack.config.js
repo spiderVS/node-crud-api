f482436da99ad6dca9016efec71b3e88be0b04ea
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2023-01-15 21:38:55 +0300

    fix: update npm-scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,47 @@
 const path = require('path');
-module.exports = {
-  entry: './src/index.ts',
-  target: "node",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+module.exports = [
+  {
+    name: 'single',
+    entry: './src/index.ts',
+    target: "node",
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+  {
+    name: 'balancer',
+    entry: './src/index-balancer.ts',
+    target: "node",
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'bundle-balancer.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
   },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+];
